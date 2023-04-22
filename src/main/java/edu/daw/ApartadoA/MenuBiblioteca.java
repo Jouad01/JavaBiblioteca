a0921a2bad6ec562d356d26411dcffd1ced8faed
jouad01 <jouardiouardi@cifpfbmoll.eu>
2023-04-22 19:43:11 +0200

    feat: agregada clase main (en proceso)

--- src/main/java/edu/daw/ApartadoA/MenuBiblioteca.java
+++ src/main/java/edu/daw/ApartadoA/MenuBiblioteca.java
@@ -1,101 +1,41 @@
-package edu.daw;
+package edu.daw.ApartadoA;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MenuBiblioteca {
     public static void main(String[] args) {
-        Biblioteca biblioteca = new Biblioteca();
-        Scanner input = new Scanner(System.in);
-        int opcion;
+        System.out.println("------Menu Biblioteca-------");
+        ArrayList <Libro> listaLibros = new ArrayList <>();
+        ArrayList <Persona> listaBibliotecarios = new ArrayList <>();
+        ArrayList <Usuario> listaUsuarios = new ArrayList <>();
+        ArrayList <Libro> listaReservas = new ArrayList <>();
 
-        do {
-            System.out.println("\n*** MENÚ PRINCIPAL ***\n");
-            System.out.println("1. Añadir libro");
-            System.out.println("2. Eliminar libro");
-            System.out.println("3. Buscar libro por ISBN");
-            System.out.println("4. Buscar libro por título");
-            System.out.println("5. Mostrar libros");
-            System.out.println("6. Mostrar libros disponibles");
-            System.out.println("0. Salir");
+        boolean isRunning = true;
+        while (isRunning) {
+            System.out.println("1. Gestionar reservas");
+            System.out.println("2. Gestionar personal");
+            System.out.println("3. Gestionar usuarios");
+            System.out.println("4. Salir");
 
-            System.out.print("\nElija una opción: ");
-            opcion = input.nextInt();
-
+            System.out.println("Introduce una opcion");
+            Scanner input = new Scanner(System.in);
+            int opcion = input.nextInt();
             switch (opcion) {
                 case 1:
-                    System.out.println("\n*** AÑADIR LIBRO ***\n");
-                    Libro.añadirLibro(biblioteca.getListaLibros());
-                    break;
-                case 2:
-                    System.out.println("\n*** ELIMINAR LIBRO ***\n");
-                    Libro.eliminarLibro(biblioteca.getListaLibros());
-                    break;
-                case 3:
-                    System.out.println("\n*** BUSCAR LIBRO POR ISBN ***\n");
-                    Libro.buscarLibroISBN(biblioteca.getListaLibros());
-                    break;
-                case 4:
-                    System.out.println("\n*** BUSCAR LIBRO POR TÍTULO ***\n");
-                    Libro.buscarLibroTitulo(biblioteca.getListaLibros());
-                    break;
-                case 5:
-                    System.out.println("\n*** MOSTRAR LIBROS ***\n");
-                    biblioteca.mostrarLibros();
-                    break;
-                case 6:
-                    System.out.println("\n*** MOSTRAR LIBROS DISPONIBLES ***\n");
-                    biblioteca.mostrarLibrosDisponibles();
-                    break;
-                case 0:
-                    System.out.println("\nGracias por utilizar nuestro sistema de gestión de bibliotecas.");
-                    break;
-                default:
-                    System.out.println("\nOpción inválida. Por favor, seleccione una opción válida.");
+                    isRunning = true;
+                    while (isRunning) {
+                        System.out.println("1. Añadir libro");
+                        System.out.println("2. Eliminar libro por ISBN");
+                        System.out.println("3. Buscar libro por ISBN");
+                        System.out.println("4. Buscar libro por titulo");
+                        System.out.println("5. Mostrar todos los libros");
+                        System.out.println("6. Volver al menu principal");
+                        System.out.println("Introduce una opcion");
+//                        en proceso
+                    }
             }
-        } while (opcion != 0);
+        }
     }
 
-    public static void gestionarPersonal(Biblioteca biblioteca) {
-        Scanner input = new Scanner(System.in);
-        int opcion;
-
-        do {
-            System.out.println("\n*** GESTIONAR PERSONAL ***\n");
-            System.out.println("1. Añadir persona");
-            System.out.println("2. Eliminar persona");
-            System.out.println("0. Salir");
-
-            System.out.print("\nElija una opción: ");
-            opcion = input.nextInt();
-
-            switch (opcion) {
-                case 1:
-                    System.out.println("\n*** AÑADIR PERSONA ***\n");
-                    Persona persona = new Persona("Juan", "Pérez", "12345678A", "123456789");
-                    Persona.agregarPersona(persona, biblioteca.getPersonal());
-                    break;
-                case 2:
-                    System.out.println("\n*** ELIMINAR PERSONA ***\n");
-                    String nif = "12345678A";
-                    Persona persona2 = null;
-                    for (Persona p : biblioteca.getPersonal()) {
-                        if (p.getNIF().equals(nif)) {
-                            persona2 = p;
-                            break;
-                        }
-                    }
-                    if (persona2 != null) {
-                        Persona.eliminarPersona(persona2, biblioteca.getPersonal());
-                    } else {
-                        System.out.println("No se encontró a la persona con ese NIF " + nif);
-                    }
-                    break;
-                case 0:
-                    System.out.println("\nVolviendo al menú principal...");
-                    break;
-                default:
-                    System.out.println("\nOpción inválida. Por favor, seleccione una opción válida.");
-            }
-        } while (opcion != 0);
-    }
 }
