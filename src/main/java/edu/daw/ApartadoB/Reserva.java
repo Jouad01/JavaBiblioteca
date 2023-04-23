cc420cd6553497fff55a321b5fc42a64f4287fdf
jouad01 <jouardiouardi@cifpfbmoll.eu>
2023-04-23 23:31:21 +0200

    feat: añadidos atributos para la clase reserva

--- src/main/java/edu/daw/ApartadoB/Reserva.java
+++ src/main/java/edu/daw/ApartadoB/Reserva.java
@@ -1,4 +1,63 @@
 package edu.daw.ApartadoB;
-
+import edu.daw.ApartadoA.Libro;
 public class Reserva {
+    private Libro libro;
+    private String fechaReserva;
+    private String horaReserva;
+
+//    constructor vacio
+    public Reserva() {
+    }
+
+//    constructor con parametros
+    public Reserva(Libro libro, String fechaReserva, String horaReserva) {
+        this.libro = libro;
+        this.fechaReserva = fechaReserva;
+        this.horaReserva = horaReserva;
+    }
+
+//    constructor copia
+    public Reserva(Reserva reserva) {
+        this.libro = reserva.libro;
+        this.fechaReserva = reserva.fechaReserva;
+        this.horaReserva = reserva.horaReserva;
+    }
+
+//    getters y setters
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public void setLibro(Libro libro) {
+        this.libro = libro;
+    }
+
+    public String getFechaReserva() {
+        return fechaReserva;
+    }
+
+    public void setFechaReserva(String fechaReserva) {
+        this.fechaReserva = fechaReserva;
+    }
+
+    public String getHoraReserva() {
+        return horaReserva;
+    }
+
+    public void setHoraReserva(String horaReserva) {
+        this.horaReserva = horaReserva;
+    }
+
+//    toString
+
+    @Override
+    public String toString() {
+        return "Reserva{" +
+                "libro=" + libro +
+                ", fechaReserva='" + fechaReserva + '\'' +
+                ", horaReserva='" + horaReserva + '\'' +
+                '}';
+    }
+
 }
